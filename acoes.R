b6f30ad89ab3ac5fdd5877267bd3479ab260b181
Rhodrigo Souza <rhdrig@gmail.com>
2022-05-17 10:51:52 +0800

    Dashboard - acoes

---
+++ acoes.R
@@ -0,0 +1,16 @@
+library(tidyquant)
+library(dplyr)
+prices <- c(
+  "BBDC4", "RENT3", "BRFS3", "BPAN4", "ELET3", "CCRO3",
+  "GGBR4", "PRIO3", "MGLU3", "SULA11"
+) %>%
+  paste0(".SA") %>%
+  tq_get(get = "stock.prices", from = " 2010-01-01") %>%
+  mutate(symbol = sub(".SA", "",symbol))
+
+
+write.table(prices, "prices.csv", sep = ",", row.names= FALSE)
+prices <- read.csv("C:/Users/USER/Documents/dashboard/prices.csv")%>%
+  mutate(date = as.Date(date))
+
+
